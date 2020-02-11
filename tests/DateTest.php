a845579b669371405d0bd5a60e45a17faa2092e4
Alvin Manansala <aomanansala@gmail.com>
2020-02-11 21:28:31 +1030

    update directory and class name

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -2,7 +2,7 @@
 
 namespace App\Test;
 
-use App\DPSDate;
+use App\DateConversion;
 use PHPUnit\Framework\TestCase;
 
 Class DateTest extends TestCase 
@@ -12,7 +12,7 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '14-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate);
+		$date = new DateConversion($firstDate, $secondDate);
 		$this->assertEquals(2, $date->getNumberOfDaysBetweenTwoDates());
 	}
 
@@ -21,7 +21,7 @@ Class DateTest extends TestCase
 		$firstDate = '10-02-2020';
 		$secondDate = '24-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate);
+		$date = new DateConversion($firstDate, $secondDate);
 		$this->assertEquals(9, $date->getNumberOfWeekdaysBetweenTwoDates());
 	}
 
@@ -30,7 +30,7 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '02-03-2020';
 
-		$date = new DPSDate($firstDate, $secondDate);
+		$date = new DateConversion($firstDate, $secondDate);
 		$this->assertEquals(2, $date->getNumberOfWeeksBetweenTwoDates());
 	}
 
@@ -39,19 +39,19 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '14-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$date = new DateConversion($firstDate, $secondDate, 'seconds');
 		$this->assertEquals(172800, $date->getNumberOfDaysBetweenTwoDates());
 
 		$firstDate = '10-02-2020';
 		$secondDate = '24-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$date = new DateConversion($firstDate, $secondDate, 'seconds');
 		$this->assertEquals(777600, $date->getNumberOfWeekdaysBetweenTwoDates());
 
 		$firstDate = '11-02-2020';
 		$secondDate = '02-03-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$date = new DateConversion($firstDate, $secondDate, 'seconds');
 		$this->assertEquals(1209600, $date->getNumberOfWeeksBetweenTwoDates());
 	}
 
@@ -60,19 +60,19 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '14-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$date = new DateConversion($firstDate, $secondDate, 'minutes');
 		$this->assertEquals(2880, $date->getNumberOfDaysBetweenTwoDates());
 
 		$firstDate = '10-02-2020';
 		$secondDate = '24-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$date = new DateConversion($firstDate, $secondDate, 'minutes');
 		$this->assertEquals(12960, $date->getNumberOfWeekdaysBetweenTwoDates());
 
 		$firstDate = '11-02-2020';
 		$secondDate = '02-03-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$date = new DateConversion($firstDate, $secondDate, 'minutes');
 		$this->assertEquals(20160, $date->getNumberOfWeeksBetweenTwoDates());
 	}
 
@@ -81,19 +81,19 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '14-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'hours');
+		$date = new DateConversion($firstDate, $secondDate, 'hours');
 		$this->assertEquals(48, $date->getNumberOfDaysBetweenTwoDates());
 
 		$firstDate = '10-02-2020';
 		$secondDate = '24-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'hours');
+		$date = new DateConversion($firstDate, $secondDate, 'hours');
 		$this->assertEquals(216, $date->getNumberOfWeekdaysBetweenTwoDates());
 
 		$firstDate = '11-02-2020';
 		$secondDate = '02-03-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'hours');
+		$date = new DateConversion($firstDate, $secondDate, 'hours');
 		$this->assertEquals(336, $date->getNumberOfWeeksBetweenTwoDates());
 	}
 
@@ -102,19 +102,19 @@ Class DateTest extends TestCase
 		$firstDate = '11-02-2020';
 		$secondDate = '14-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'years');
+		$date = new DateConversion($firstDate, $secondDate, 'years');
 		$this->assertEquals(0.0054794520547945, $date->getNumberOfDaysBetweenTwoDates());
 
 		$firstDate = '10-02-2020';
 		$secondDate = '24-02-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'years');
+		$date = new DateConversion($firstDate, $secondDate, 'years');
 		$this->assertEquals(0.024657534246575, $date->getNumberOfWeekdaysBetweenTwoDates());
 
 		$firstDate = '11-02-2020';
 		$secondDate = '02-03-2020';
 
-		$date = new DPSDate($firstDate, $secondDate, 'years');
+		$date = new DateConversion($firstDate, $secondDate, 'years');
 		$this->assertEquals(0.038356164383562, $date->getNumberOfWeeksBetweenTwoDates());
 	}
 
@@ -123,7 +123,7 @@ Class DateTest extends TestCase
 		$firstDate = array('date' => '11-02-2020', 'timezone' => 'Europe/Amsterdam');
 		$secondDate = array('date' => '14-02-2020', 'timezone' => 'Australia/Adelaide');
 
-		$date = new DPSDate($firstDate, $secondDate);
+		$date = new DateConversion($firstDate, $secondDate);
 
 		$this->assertEquals($firstDate['timezone'], $date->firstDate->timezone);
 		$this->assertEquals($secondDate['timezone'], $date->secondDate->timezone);

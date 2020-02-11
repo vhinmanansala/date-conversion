ce49c1d8b859f6ad4d2762a0a8a14e0a053dd3fb
Alvin Manansala <aomanansala@gmail.com>
2020-02-11 20:50:05 +1030

    add conversion for seconds and minutes

--- src/DPSDate.php
+++ src/DPSDate.php
@@ -8,22 +8,58 @@ class DPSDate
 {
 	private $firstDate;
 	private $secondDate;
+	private $conversion;
 
-	public function __construct($firstDate, $secondDate)
+	public function __construct($firstDate, $secondDate, $conversion = null)
 	{
 		$this->firstDate = Carbon::create($firstDate);
 		$this->secondDate = Carbon::create($secondDate);
+		$this->conversion = $conversion;
 	}
 
 	public function getNumberOfDaysBetweenTwoDates()
 	{
-		return $this->firstDate->diffInDays($this->secondDate) - 1;
+		$numberOfDays = $this->firstDate->diffInDays($this->secondDate) - 1;
+
+		if (! is_null($this->conversion)) {
+			return $this->{$this->conversion}($numberOfDays);
+		}
+
+		return $numberOfDays;
 	}
 
 	public function getNumberOfWeekdaysBetweenTwoDates()
 	{
-		return $this->firstDate->diffInDaysFiltered(function(Carbon $date) use (&$days) {
+		$numberOfWeekDays =  $this->firstDate->diffInDaysFiltered(function(Carbon $date) use (&$days) {
 			return ! $date->isWeekend();
 		}, $this->secondDate) - 1;
+
+		if (! is_null($this->conversion)) {
+			return $this->{$this->conversion}($numberOfWeekDays);
+		}
+
+		return $numberOfWeekDays;
+	}
+
+	public function getNumberOfWeeksBetweenTwoDates()
+	{
+		$numberOfWeeks = $this->firstDate->diffInWeeks($this->secondDate);
+
+		if (! is_null($this->conversion)) {
+			$days = $numberOfWeeks * 7;
+			return $this->{$this->conversion}($days);
+		}
+
+		return $numberOfWeeks;
+	}
+
+	private function seconds($numberOfDays)
+	{
+		return $numberOfDays * 86400;
+	}
+
+	private function minutes($numberOfDays)
+	{
+		return $numberOfDays * 1440;
 	}
 }
--- tests/DateTest.php
+++ tests/DateTest.php
@@ -24,4 +24,55 @@ Class DateTest extends TestCase
 		$date = new DPSDate($firstDate, $secondDate);
 		$this->assertEquals(9, $date->getNumberOfWeekdaysBetweenTwoDates());
 	}
+
+	public function test_it_can_get_number_of_weeks_between_two_dates()
+	{
+		$firstDate = '11-02-2020';
+		$secondDate = '02-03-2020';
+
+		$date = new DPSDate($firstDate, $secondDate);
+		$this->assertEquals(2, $date->getNumberOfWeeksBetweenTwoDates());
+	}
+
+	public function test_it_can_convert_the_result_to_seconds()
+	{
+		$firstDate = '11-02-2020';
+		$secondDate = '14-02-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$this->assertEquals(172800, $date->getNumberOfDaysBetweenTwoDates());
+
+		$firstDate = '10-02-2020';
+		$secondDate = '24-02-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$this->assertEquals(777600, $date->getNumberOfWeekdaysBetweenTwoDates());
+
+		$firstDate = '11-02-2020';
+		$secondDate = '02-03-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'seconds');
+		$this->assertEquals(1209600, $date->getNumberOfWeeksBetweenTwoDates());
+	}
+
+	public function test_it_can_convert_the_result_to_minutes()
+	{
+		$firstDate = '11-02-2020';
+		$secondDate = '14-02-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$this->assertEquals(2880, $date->getNumberOfDaysBetweenTwoDates());
+
+		$firstDate = '10-02-2020';
+		$secondDate = '24-02-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$this->assertEquals(12960, $date->getNumberOfWeekdaysBetweenTwoDates());
+
+		$firstDate = '11-02-2020';
+		$secondDate = '02-03-2020';
+
+		$date = new DPSDate($firstDate, $secondDate, 'minutes');
+		$this->assertEquals(20160, $date->getNumberOfWeeksBetweenTwoDates());
+	}
 }
